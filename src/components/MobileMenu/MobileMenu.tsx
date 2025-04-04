a9845bade2fa5d0641316e127698546815d54455
ZimmerMike <angelmetalhj@gmail.com>
2025-04-04 03:50:33 -0600

    Remove glow effect for mobile touch devices, reduced glow size, added styles in sidebar and transition in mobile menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -17,22 +17,21 @@ export default function MobileMenu() {
       </button>
 
       {/* Menú lateral flotante */}
-      {menuOpen && (
-        <div className="fixed inset-0 bg-black/70 z-50 flex justify-end md:hidden">
-          <div className="bg-slate-900 px-6 py-2 h-fit w-1/4 shadow-lg absolute bottom-0 rounded-t-2xl">
-            <ul className="flex flex-col gap-4">
-              <NavbarItem href="/experience" icon="pi-briefcase" label="Experiencia" />
-              <NavbarItem href="/education" icon="pi-book" label="Educación" />
-              <NavbarItem href="/certifications" icon="pi-check-circle" label="Certificaciones" />
-            </ul>
-            <div className="flex justify-center items-center px-6 py-2 mt-4">
-              <button onClick={() => setMenuOpen(false)}>
-                <i className="pi pi-times text-2xl" />
-              </button>
-            </div>
+      <div className={`fixed inset-0 z-50 flex justify-end md:hidden transition-all duration-300 ${menuOpen ? 'bg-black/70 visible opacity-100' : 'opacity-0 invisible'}`}>
+        <div className={`bg-slate-900 px-6 py-2 w-1/4 sm:w-1/2 md:w-1/4 shadow-lg absolute bottom-0 rounded-t-2xl transition-transform duration-500 ${menuOpen ? 'translate-y-0' : 'translate-y-full'}`}>
+          <ul className="flex flex-col gap-4">
+            <NavbarItem href="/experience" icon="pi-briefcase" label="Experiencia" />
+            <NavbarItem href="/education" icon="pi-book" label="Educación" />
+            <NavbarItem href="/certifications" icon="pi-check-circle" label="Certificaciones" />
+          </ul>
+
+          <div className="flex justify-center items-center px-6 py-2 mt-4">
+            <button onClick={() => setMenuOpen(false)}>
+              <i className="pi pi-times text-2xl" />
+            </button>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
